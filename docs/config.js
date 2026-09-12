53173467b0189a1ff90d467cccda32e1265d1f2f
arya <minatosheller@gmail.com>
2026-09-12 16:48:02 +0700

    chore: add branch-pages site config

---
+++ docs/config.js
@@ -0,0 +1,5 @@
+window.BDZ_CONFIG={
+  SITE_URL:'https://aryaxynk.github.io/bdzteam',
+  SUPABASE_URL:'https://nklukqriopezsoalnghm.supabase.co',
+  SUPABASE_PUBLISHABLE_KEY:'sb_publishable_RUE5iV8GqoVCFxjVt5ZBpg_FlTC1v-0'
+};
